82b26a8c376614d118bdddcb11952188bee32de6
maziyar <maziyar@controller.nc-torch.escra-pg0.utah.cloudlab.us>
2023-03-30 19:02:41 -0600

    serverless dnn using pytorch rpc and nightcore functions as workers has been completed

---
+++ workloads/ServerlessDNN/src/worker1/python/build/src/base/logging.d
@@ -0,0 +1,11 @@
+build/src/base/logging.o: src/base/logging.cpp src/base/logging.h \
+ src/base/macro.h deps/fmt/include/fmt/format.h \
+ deps/fmt/include/fmt/core.h deps/fmt/include/fmt/format-inl.h \
+ deps/fmt/include/fmt/format.h deps/fmt/include/fmt/core.h
+src/base/logging.h:
+src/base/macro.h:
+deps/fmt/include/fmt/format.h:
+deps/fmt/include/fmt/core.h:
+deps/fmt/include/fmt/format-inl.h:
+deps/fmt/include/fmt/format.h:
+deps/fmt/include/fmt/core.h:
---
+++ workloads/ServerlessDNN/src/worker2/main.py
@@ -0,0 +1,228 @@
+import sys
+import faas
+import os
+import threading
+import time
+from functools import wraps
+
+import torch
+import torch.nn as nn
+import torch.distributed.autograd as dist_autograd
+import torch.distributed.rpc as rpc
+import torch.multiprocessing as mp
+import torch.optim as optim
+from torch.distributed.optim import DistributedOptimizer
+from torch.distributed.rpc import RRef
+
+from torchvision.models.resnet import Bottleneck
+import ast
+num_classes = 1000
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+class ResNetBase(nn.Module):
+    def __init__(self, block, inplanes, num_classes=1000,
+                 groups=1, width_per_group=64, norm_layer=None):
+        super(ResNetBase, self).__init__()
+
+        self._lock = threading.Lock()
+        self._block = block
+        self._norm_layer = nn.BatchNorm2d
+        self.inplanes = inplanes
+        self.dilation = 1
+        self.groups = groups
+        self.base_width = width_per_group
+
+    def _make_layer(self, planes, blocks, stride=1):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if stride != 1 or self.inplanes != planes * self._block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * self._block.expansion, stride),
+                norm_layer(planes * self._block.expansion),
+            )
+
+        layers = []
+        layers.append(self._block(self.inplanes, planes, stride, downsample, self.groups,
+                                  self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * self._block.expansion
+        for _ in range(1, blocks):
+            layers.append(self._block(self.inplanes, planes, groups=self.groups,
+                                      base_width=self.base_width, dilation=self.dilation,
+                                      norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def parameter_rrefs(self):
+        r"""
+        Create one RRef for each parameter in the given local module, and return a
+        list of RRefs.
+        """
+        return [RRef(p) for p in self.parameters()]
+
+
+class ResNetShard1(ResNetBase):
+    """
+    The first part of ResNet.
+    """
+    def __init__(self, device, *args, **kwargs):
+        super(ResNetShard1, self).__init__(
+            Bottleneck, 64, num_classes=num_classes, *args, **kwargs)
+
+        self.device = device
+        self.seq = nn.Sequential(
+            nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False),
+            self._norm_layer(self.inplanes),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            self._make_layer(64, 3),
+            self._make_layer(128, 4, stride=2)
+        )
+        # .to(self.device)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x_rref):
+        x = x_rref.to_here()
+        # .to(self.device)
+        with self._lock:
+            out =  self.seq(x)
+        return out.cpu()
+
+
+class ResNetShard2(ResNetBase):
+    """
+    The second part of ResNet.
+    """
+    def __init__(self, device, *args, **kwargs):
+        super(ResNetShard2, self).__init__(
+            Bottleneck, 512, num_classes=num_classes, *args, **kwargs)
+
+        self.device = device
+        self.seq = nn.Sequential(
+            self._make_layer(256, 6, stride=2),
+            self._make_layer(512, 3, stride=2),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        # .to(self.device)
+
+        self.fc =  nn.Linear(512 * self._block.expansion, num_classes)
+        # .to(self.device)
+
+    def forward(self, x_rref):
+        x = x_rref.to_here()
+        # .to(self.device)
+        with self._lock:
+            out = self.fc(torch.flatten(self.seq(x), 1))
+        return out.cpu()
+
+
+class DistResNet50(nn.Module):
+    """
+    Assemble two parts as an nn.Module and define pipelining logic
+    """
+    def __init__(self, split_size, workers, ranks, *args, **kwargs):
+        super(DistResNet50, self).__init__()
+
+        self.split_size = split_size
+
+        # Put the first part of the ResNet50 on workers[0]
+        self.p1_rref = rpc.remote(
+            workers[0],
+            ResNetShard1,
+            args = (ranks[0],) + args,
+            kwargs = kwargs
+        )
+
+        # Put the second part of the ResNet50 on workers[1]
+        self.p2_rref = rpc.remote(
+            workers[1],
+            ResNetShard2,
+            args = (ranks[1],) + args,
+            kwargs = kwargs
+        )
+
+    def forward(self, xs):
+        # Split the input batch xs into micro-batches, and collect async RPC
+        # futures into a list
+        out_futures = []
+        for x in iter(xs.split(self.split_size, dim=0)):
+            x_rref = RRef(x)
+            y_rref = self.p1_rref.remote().forward(x_rref)
+            z_fut = self.p2_rref.rpc_async().forward(y_rref)
+            out_futures.append(z_fut)
+
+        # collect and cat all output tensors into one tensor.
+        return torch.cat(torch.futures.wait_all(out_futures))
+
+    def parameter_rrefs(self):
+        remote_params = []
+        remote_params.extend(self.p1_rref.remote().parameter_rrefs().to_here())
+        remote_params.extend(self.p2_rref.remote().parameter_rrefs().to_here())
+        return remote_params
+
+
+#########################################################
+#                   Run RPC Processes                   #
+#########################################################
+
+
+def run_worker(rank, world_size, num_split):
+    # Higher timeout is added to accommodate for kernel compilation time in case of ROCm.
+    options = rpc.TensorPipeRpcBackendOptions(num_worker_threads=256, rpc_timeout=300, _transports=["uv"])
+
+    if rank == 0:
+        rpc.init_rpc(
+            "master",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        run_master(num_split)
+    else:
+        rpc.init_rpc(
+            f"worker{rank}",
+            rank=rank,
+            world_size=world_size,
+            rpc_backend_options=options
+        )
+        pass
+
+    # block until all rpcs finish
+    rpc.shutdown()
+
+
+def handler_factory(func_name):
+    async def foo_handler(ctx, input_):
+        # bar_output = await ctx.invoke_func('Bar', input_)
+        dict_str = input_.decode("UTF-8")
+        mydata = ast.literal_eval(dict_str)
+        sys.stderr.write('rank: {}\n'.format(mydata['rank']))
+
+        rank = int(mydata['rank'])
+        world_size = 3
+        num_split = 1
+
+        tik = time.time()
+        run_worker(rank, world_size, num_split)
+        tok = time.time()
+
+        return input_ + ', Foo\n'.encode()
+
+    if func_name == 'wrk2':
+        return foo_handler
+    else:
+        sys.stderr.write('Unknown function: {}\n'.format(func_name))
+        sys.exit(1)
+
+if __name__ == '__main__':
+    faas.serve_forever(handler_factory)
